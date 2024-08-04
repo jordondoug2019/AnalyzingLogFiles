cf65fbad99c478910a755e0f453fe35590373b6f
Jordon <jdouglas2015@gmail.com>
2024-08-04 21:31:43 +0000

    adding sus log

---
+++ suspicious_activity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Read through each line of the log file
+#search for keywords that indicate potential suspicious login 
+#IF line contains this keyword, add to suspicious_actvity.log 
+#automate to run daily 
+
+
+logs="/var/log/auth_log.log"
+suspicious_activity="/var/log/suspicious_activity.log"
+
+
+for each in "$logs" 
+do
+    grep "Unauthorized" "$logs" >> "$suspicious_activity"
+done
+
+echo "$each  logged on $(date)"
